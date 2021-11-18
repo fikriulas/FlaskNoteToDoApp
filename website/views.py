0cd7e1edadd4726fd797cb133e2bd1ad711471fc
Fikri Ulaş Osmanağaoğlu <2014010213010@ogrenci.karabuk.edu.tr>
2021-11-18 10:41:23 +0300

    Note sayfası template'i oluşturuldu.

--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request, flash
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template('/home.html')
+
+@views.route('/note')
+def note():
+    return render_template('note.html')
